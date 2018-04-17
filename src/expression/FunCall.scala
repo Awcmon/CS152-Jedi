37a6673c0fc479541fae57335beae11470a90c3a
Awcmon <adcarbine@yahoo.com>
2018-04-17 14:33:27 -0700

    Some jedi 1.0 stuff

---
+++ src/expression/FunCall.scala
@@ -0,0 +1,11 @@
+package expression
+
+import value._
+
+case class FunCall(val operator: Identifier, val operands: List[Expression]) extends Expression
+{
+  def execute(env: Environment): Value = {
+    val args: List[Value] = operands.map(_.execute(env))
+    alu.execute(operator, args)
+  }
+}
