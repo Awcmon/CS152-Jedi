e4646bf1aa07867789c085239eca14d9e6307749
Awcmon <adcarbine@yahoo.com>
2018-04-26 17:26:00 -0700

    Start of Jedi 2.0. Add parsers.

---
+++ src/context/Jedi2Parsers.scala
@@ -0,0 +1,27 @@
+package context
+
+import scala.util.parsing.combinator._
+import expression._
+import value._
+
+class Jedi2Parsers extends Jedi1Parsers {
+  
+  // params parser
+  // a parameter list is zero or more comma-separated identifiers bracketed by parentheses:
+  // params ::= "(" ~ (identifier ~ ("," ~ identifier)*)? ~ ")"
+  def operands: Parser[List[Expression]] = "(" ~ opt(identifier ~ rep("," ~> identifier)) ~ ")" ^^ {
+    case "("~Some(id~(more))~")" => id::more
+  }
+  
+  // lambda parser
+  // lambda ::= "lambda" ~ params ~ expression
+  
+  // block parser
+  // a block is one or more semi-colon separated expressions bracketed by curly braces:
+  // block ::= "{" ~ expression ~ (";" ~ expression)* ~ "}"
+  
+  
+  // override of term parser
+  override def term: Parser[Expression]  = lambda | funCall | block | literal | "("~>expression<~")"
+}
+
