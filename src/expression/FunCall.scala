bbe345624b8bbba90f3cdf5c6ff12ac1d509c315
Awcmon <adcarbine@yahoo.com>
2018-04-26 22:35:12 -0700

    FunCall modified to allow for user funcs

--- src/expression/FunCall.scala
+++ src/expression/FunCall.scala
@@ -6,7 +6,17 @@ import context._
 case class FunCall(val operator: Identifier, val operands: List[Expression]) extends Expression
 {
   def execute(env: Environment): Value = {
-    val args: List[Value] = operands.map(_.execute(env))
-    alu.execute(operator, args)
+    val args: List[Value] = operands.map(_.execute(env)) //eager execution
+    //alu.execute(operator, args)
+    try
+    {
+      val maybeClosure = operator.execute(env)
+      if(!maybeClosure.isInstanceOf[Closure]) throw new TypeException("undefined")
+      else maybeClosure.asInstanceOf[Closure](args)
+    }
+    catch
+    {
+      case e: TypeException => alu.execute(operator, args)
+    }
   }
 }
