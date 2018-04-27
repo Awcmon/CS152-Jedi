f12e9b266774e58adc90a8de6d0b63280373c855
Awcmon <adcarbine@yahoo.com>
2018-04-26 22:54:19 -0700

    change notif to ok

--- src/expression/Declaration.scala
+++ src/expression/Declaration.scala
@@ -5,5 +5,5 @@ import value._
 
 case class Declaration(val id: Identifier, val exp: Expression) extends Expression
 {
-  def execute(env: Environment) = {env(id) = exp.execute(env); Notification.DONE;}
+  def execute(env: Environment) = {env(id) = exp.execute(env); Notification.OK;}
 }
