ed38625444a99b4aff69429c2954962794917b1b
Awcmon <adcarbine@yahoo.com>
2018-04-26 22:43:47 -0700

    finish jedi 2 parsers

--- src/context/Jedi2Parsers.scala
+++ src/context/Jedi2Parsers.scala
@@ -9,17 +9,23 @@ class Jedi2Parsers extends Jedi1Parsers {
   // params parser
   // a parameter list is zero or more comma-separated identifiers bracketed by parentheses:
   // params ::= "(" ~ (identifier ~ ("," ~ identifier)*)? ~ ")"
-  def operands: Parser[List[Expression]] = "(" ~ opt(identifier ~ rep("," ~> identifier)) ~ ")" ^^ {
+  def params: Parser[List[Identifier]] = "(" ~ opt(identifier ~ rep("," ~> identifier)) ~ ")" ^^ {
     case "("~Some(id~(more))~")" => id::more
   }
   
   // lambda parser
   // lambda ::= "lambda" ~ params ~ expression
+  def lambda: Parser[Expression] = "lambda" ~ params ~ expression ^^
+  {
+    case "lambda" ~ params ~ expression => Lambda(params, expression)
+  }
   
   // block parser
   // a block is one or more semi-colon separated expressions bracketed by curly braces:
   // block ::= "{" ~ expression ~ (";" ~ expression)* ~ "}"
-  
+    def block: Parser[Expression] = "{" ~ expression ~ rep(";" ~> expression) ~ "}" ^^ {
+    case "{" ~ exp ~ more ~ "}" => Block(exp::more)
+  }
   
   // override of term parser
   override def term: Parser[Expression]  = lambda | funCall | block | literal | "("~>expression<~")"
