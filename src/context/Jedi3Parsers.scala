e9a985260efa9215ce8b40bf8bcf69d29c191c6c
Awcmon <adcarbine@yahoo.com>
2018-05-08 17:33:02 -0700

    parsers

--- src/context/Jedi3Parsers.scala
+++ src/context/Jedi3Parsers.scala
@@ -7,17 +7,19 @@ import value._
 class Jedi3Parsers extends Jedi2Parsers {
   
   // assignment ::= identifier ~ "=" ~ expression
-   def assignment: Parser[Expression] = identifier ~ "=" ~ expression ^^ {
-     case id ~ "=" ~ exp => ???
+   def assignment: Parser[Assignment] = identifier ~ "=" ~ expression ^^ {
+     case id ~ "=" ~ exp => Assignment(id, exp)
    }
   
   // iteration ::= "while" ~ "(" ~ expression ~ ")" ~ expression
-  def iteration: Parser[Conditional] = "while" ~ "(" ~ expression ~ ")" ~ expression ^^ {
-     case "while"~"("~cond~")"~exp => ???
+  def iteration: Parser[Iteration] = "while" ~ "(" ~ expression ~ ")" ~ expression ^^ {
+     case "while"~"("~cond~")"~exp => Iteration(cond, exp)
    }
   
   // dereference ::= "[" ~ expression ~ "]"
-  
+  def dereference: Parser[Expression] = "[" ~ expression ~ "]" ^^ {
+    case "[" ~ expression ~ "]" => expression
+  }
 
   override def expression: Parser[Expression] = declaration | conditional | iteration | disjunction | failure("Invalid expression")
   override def term: Parser[Expression]  = lambda | funCall | block | assignment | dereference | literal | "("~>expression<~")"
