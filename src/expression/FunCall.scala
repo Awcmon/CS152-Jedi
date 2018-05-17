ea53cb8603d94fda7da3ef57eece88d34b9d19d3
Awcmon <adcarbine@yahoo.com>
2018-05-17 00:48:55 -0700

    Revert "attempt to add passByText and Name"
    This reverts commit e5a713c7b9d3ad35ca9e39a3b832c353438ec18f.

--- src/expression/FunCall.scala
+++ src/expression/FunCall.scala
@@ -6,13 +6,7 @@ import context._
 case class FunCall(val operator: Identifier, val operands: List[Expression]) extends Expression
 {
   def execute(env: Environment): Value = {
-    val args: List[Value] = if(Flags.parameterPassing == Flags.passByText)
-          operands.map(Text(_))
-        else if(Flags.parameterPassing == Flags.passByName)
-          operands.map(_.execute(env))
-        else
-          operands.map(_.execute(env)) //eager execution
-    
+    val args: List[Value] = operands.map(_.execute(env)) //eager execution
     //alu.execute(operator, args)
     try
     {
@@ -22,7 +16,7 @@ case class FunCall(val operator: Identifier, val operands: List[Expression]) ext
     }
     catch
     {
-      case e: UndefinedException => alu.execute(operator, operands.map(_.execute(env)))
+      case e: UndefinedException => alu.execute(operator, args)
       //case e: TypeException => alu.execute(operator, args)
     }
   }
--- src/value/Text.scala
+++
@@ -1,6 +0,0 @@
-package value
-
-import expression._
-import context._
-
-case class Text(val body: Expression) extends Value
