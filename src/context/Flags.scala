ea3585f01a53d864867e967d6d178dc977e131a3
Awcmon <adcarbine@yahoo.com>
2018-05-17 01:32:14 -0700

    implemented passbytext (works)

--- src/context/Flags.scala
+++ src/context/Flags.scala
@@ -4,6 +4,6 @@ object Flags {
   val passByValue = 1
   val passByName = 2
   val passByText = 3
-  var parameterPassing = passByName
+  var parameterPassing = passByText
   var useStaticScopeRule = true
 }
--- src/expression/Identifier.scala
+++ src/expression/Identifier.scala
@@ -4,7 +4,7 @@ import context._
 import value._
 
 case class Identifier(val name: String) extends Expression {
-  def execute(env: Environment) = env(this)
+  def execute(env: Environment) = if(env(this).isInstanceOf[Text]) env(this).asInstanceOf[Text].body.execute(env) else env(this)
   override def toString = name
     
 }
